c2da1b74a03d31a45b58a56b95a5f10d577af14c
peita1 <peita1@peita1deMacBook-Pro.local>
2019-01-15 11:18:51 +0800

    modify dockerfile apt-get install

--- actions/action-run-test/entrypoint.sh
+++ actions/action-run-test/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
+
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "Start install dependency" | sudo tee /etc/apt/sources.list.d/yarn.list
