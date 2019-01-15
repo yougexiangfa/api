314c27c205f302aba2a182399aa8eeda2fa645c0
peita1 <peita1@peita1deMacBook-Pro.local>
2019-01-15 11:05:37 +0800

    modify dockerfile to install latest ruby version

--- actions/action-run-test/entrypoint.sh
+++ actions/action-run-test/entrypoint.sh
@@ -1,3 +1,21 @@
 #!/bin/sh -l
 
-sh -c "echo $*"
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "Start install dependency" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn	
+
+cd
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
+
+rbenv install 2.5.3
+rbenv global 2.5.3
+ruby -v
