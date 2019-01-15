5cbb9757bb3c2f3bddc3b6f13373188ca922a620
peita1 <peita1@peita1deMacBook-Pro.local>
2019-01-15 15:49:59 +0800

    move ruby package to inner directory

--- actions/action-run-test/entrypoint.sh
+++ actions/action-run-test/entrypoint.sh
@@ -1,22 +1,25 @@
 #!/bin/sh -l
 
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+# curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+# curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-echo "Complete install dependency"
+# echo "Complete install dependency"
 
-cd
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+# /bin/bash
+# cd
+# git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+# echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+# exec $SHELL
+
+# git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+# echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+# exec $SHELL
+
+# rbenv install 2.5.3
+# rbenv global 2.5.3
+echo 'export PATH="/ruby-2.5.1/bin:$PATH"' >> ~/.bashrc
 exec $SHELL
-
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-exec $SHELL
-
-rbenv install 2.5.3
-rbenv global 2.5.3
 ruby -v
